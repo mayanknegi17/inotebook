e125b4883fbba34d5e4f36ae75bed843ae95b1ac
Mayank Singh <mayank@turningcloud.in>
2022-01-06 00:48:24 +0530

    token

--- backend/models/User.js
+++ backend/models/User.js
@@ -22,7 +22,6 @@ const UserSchema = new Schema({
     },
 });
 
-const User = mongoose.model('user', UserSchema); 
-User.createIndexes();
+const User = mongoose.model('user', UserSchema);
 
 module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,15 +2,17 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-
+const JWT_SECRET = 'mayankisab$oy'
 
 // Create a user useing POST "/api/auth/". does not require auth
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be 5 characters').isLength({ min: 5 }),
-], async (req, res)=> {
+], async (req, res)=>{
     // If there are errors, return bed requests
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -18,17 +20,34 @@ router.post('/createuser', [
     }
     // Check whether the user with the same email
     try {
-        let user = await User.findOne({email: req.body.email});
+        let user = User.findOne({email: req.body.email});
+        console.log(user);
         if (user){
             return res.status(400).json({ error: "Sorry a user with this email already exists" })
         }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt)
+
+        // Create a user
         user = await User.create({
             name: req.body.name,
+            password: secPass,
             email: req.body.email,
-            password: req.body.password,
-        })
+        });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        console.log(authtoken);
+
+
+        res.json({authtoken})
     } catch (error) {
         console.error(error.message);
+        res.status(500).send("Some error occured")
     }
     
 
