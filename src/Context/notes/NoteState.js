900c0d5baaab914a9301591363f7a03dad3e680d
Mayank Singh <mayank@turningcloud.in>
2022-11-20 13:20:54 +0530

    port changes

--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -3,7 +3,7 @@ import NoteContext from './NoteContext';
 import {useState} from 'react';
 
 const NoteState = (props) => {
-    const host = "http://localhost:5000"
+    const host = "http://localhost:8081"
     const notesInitial = []
     const [notes, setNotes] = useState(notesInitial)
 
